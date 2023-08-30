9cc3da1895a41cb188bd798407a164f63eb0a261
Ekete Emmanuel <emmanuelugochukwuekete@gmail.com>
2023-08-30 21:08:12 +0100

    sending respone

--- Nodejs/NodeJS_Basics/core_Modules/Node_Server/response.js
+++ Nodejs/NodeJS_Basics/core_Modules/Node_Server/response.js
@@ -13,3 +13,12 @@ const server = http.createServer((req, res) => {
 });
 
 server.listen(3000);
+
+/**
+ * NOTE:
+ * On both requests and responses, Http headers are added to transport metadata from A to B.
+
+The following article provides a great overview of available headers and their role: https://developer.mozilla.org/en-US/docs/Web/HTTP/Headers
+
+Whilst this article is a great resource, especially to dive deeper, please don't learn this list by heart though! You'll encounter many of these headers throughout the course and I'll explain them when we need them.
+ */
