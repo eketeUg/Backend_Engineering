a1c7bd55f6ba114cd35503c3e4488b8a385f5fb1
Ekete Emmanuel <emmanuelugochukwuekete@gmail.com>
2023-08-30 21:06:09 +0100

    sending respone

--- Nodejs/NodeJS_Basics/core_Modules/Node_Server/response.js
+++ Nodejs/NodeJS_Basics/core_Modules/Node_Server/response.js
@@ -2,6 +2,14 @@ const http = require("http");
 
 const server = http.createServer((req, res) => {
   res.setHeader("Content-Type", "text/html");
+  res.write(
+    ` <html>
+      <body>
+        <h1>Hello world!</h1>
+      </body>
+    </html>`
+  );
+  res.end();
 });
 
 server.listen(3000);
