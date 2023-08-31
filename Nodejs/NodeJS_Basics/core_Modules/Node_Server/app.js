9468520a592c47718935cfa1014c1472d12c649a
Ekete Emmanuel <emmanuelugochukwuekete@gmail.com>
2023-08-31 21:06:14 +0100

    using routes.js to handle routing

--- Nodejs/NodeJS_Basics/core_Modules/Node_Server/app.js
+++ Nodejs/NodeJS_Basics/core_Modules/Node_Server/app.js
@@ -1,6 +1,9 @@
 // global module - http
 const http = require("http");
 
+// importin the route
+const routes = require("./routes");
+
 // function reqListerner(req, res) {}
 // // creating a server
 // http.createServer(reqListerner);
@@ -15,9 +18,12 @@ const http = require("http");
 
 // to access some request fields like url, method and headers
 
-http
-  .createServer((req, res) => {
-    console.log(req.url, req.method);
-    // process.exit();
-  })
-  .listen(3000);
+// http
+//   .createServer((req, res) => {
+//     console.log(req.url, req.method);
+//     // process.exit();
+//   })
+//   .listen(3000);
+
+// using route
+http.createServer(routes).listen(3000);
--- Nodejs/NodeJS_Basics/core_Modules/Node_Server/routes.js
+++ Nodejs/NodeJS_Basics/core_Modules/Node_Server/routes.js
@@ -1,37 +1,43 @@
 // using the node module system (routes.js to handle routings)
 
 const fs = require("fs");
-
-if (url == "/") {
-  res.write(`<html><body>
+// connecting the app.js with routes.js
+const requestHandler = (req, res) => {
+  const url = req.url;
+  const method = req.method;
+  if (url === "/") {
+    res.write(`<html><body>
         <form action="/message" method="POST"><input type="text" name="message"><button type="submit">Send</button></form></body>
         </html>`);
-  return res.end();
-}
-if (url === "/message" && method === "POST") {
-  const body = [];
-  // register event listerner to listen to a data event
-  req.on("data", (chunk) => {
-    console.log(chunk);
-    body.push(chunk);
-  });
-
-  // register another event listener to be fired when the on event listerner is done with parsing the data
-  return req.on("end", () => {
-    // using the Buffer method to join all the body message and turn them into a string.
-    const parsedBody = Buffer.concat(body).toString();
-    console.log(parsedBody);
-    const message = parsedBody.split("=")[1];
-    // create a new file and save users message
-    fs.writeFileSync("message.txt", message);
-    res.statusCode = 302;
-    res.setHeader("Location", "/");
-
     return res.end();
-  });
-}
-res.setHeader("Content-Type", "text/html");
-res.write(`<html><body>
+  }
+  if (url === "/message" && method === "POST") {
+    const body = [];
+    // register event listerner to listen to a data event
+    req.on("data", (chunk) => {
+      console.log(chunk);
+      body.push(chunk);
+    });
+
+    // register another event listener to be fired when the on event listerner is done with parsing the data
+    return req.on("end", () => {
+      // using the Buffer method to join all the body message and turn them into a string.
+      const parsedBody = Buffer.concat(body).toString();
+      console.log(parsedBody);
+      const message = parsedBody.split("=")[1];
+      // create a new file and save users message
+      fs.writeFileSync("message.txt", message);
+      res.statusCode = 302;
+      res.setHeader("Location", "/");
+
+      return res.end();
+    });
+  }
+  res.setHeader("Content-Type", "text/html");
+  res.write(`<html><body>
         <h1>Hello !!!</h1></body>
         </html>`);
-res.end();
+  res.end();
+};
+
+module.exports = requestHandler;
