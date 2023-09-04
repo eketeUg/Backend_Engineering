8e0678c9d27ac2c4e5516acefecc6e2cb2d9351c
Ekete Emmanuel <emmanuelugochukwuekete@gmail.com>
2023-09-05 00:19:56 +0100

    middleware intro

--- Nodejs/Express/Basics/server.js
+++ Nodejs/Express/Basics/server.js
@@ -1,6 +1,11 @@
 const http = require("http");
 const express = require("express");
 
+// middleware
+app.use((req, res, next) => {
+  console.log("first middleware");
+});
+
 const app = express();
 
 app.listen(5000);
